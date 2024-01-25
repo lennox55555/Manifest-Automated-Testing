c54aabf3cc7c2071316ae139c9ea1760fe6b47e4
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-01-25 10:05:48 -0700

    Delete cypress/e2e/toggle-fullscreen.cy.js

--- cypress/e2e/toggle-fullscreen.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Manifest Bar Tests', () => {
-  it('should toggle the manifest bar', () => {
-    cy.visit('http://localhost/dist/');
-    cy.get('#fullscreen-menu').click();
-    //cy.get('#manifest-list').should('not.be.visible');
-    cy.get('#fullscreen-menu').click();
-   // cy.get('#manifest-list').should('be.visible');
-  });
-});
