d4c3f39e20452031d758ac02d3e0eb0084fc74d9
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-01-25 10:06:25 -0700

    Delete cypress/e2e/visualization-chord.cy.js

--- cypress/e2e/visualization-chord.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Visualization Option Tests', () => {
-  it('should change visualization options', () => {
-    cy.visit('http://localhost/dist/');
-    cy.get('#viz-choices').select('Chord');
-  });
-});
