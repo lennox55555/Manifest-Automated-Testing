0cf7e0bea1a061355848563e4e5baf9ce6ca8ff7
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-01-25 10:06:16 -0700

    Delete cypress/e2e/toggle-hamburger.cy.js

--- cypress/e2e/toggle-hamburger.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Manifest Bar Tests', () => {
-  it('should toggle the manifest bar', () => {
-    cy.visit('http://localhost/dist/');
-    cy.get('#minfo-hamburger').click();
-    cy.get('#minfodetail').should('be.visible');
-    cy.get('#minfo-hamburger').click();
-    cy.get('#minfodetail').should('not.be.visible');
-  });
-});
