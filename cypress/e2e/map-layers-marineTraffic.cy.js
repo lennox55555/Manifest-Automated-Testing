c42bf0fc11fcb6a68f684e31388dda06a72d4f7e
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-01-25 10:05:17 -0700

    Delete cypress/e2e/map-layers-marineTraffic.cy.js

--- cypress/e2e/map-layers-marineTraffic.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Map Layer Toggle Tests', () => {
-  it('should toggle map layers', () => {
-    cy.visit('http://localhost/dist/');
-    cy.get('#mapmenu').click();
-    cy.get('#mapmenu-window').should('be.visible');
-    cy.get('input[type="checkbox"][value="marine"]').click({ force: true });
-
-  });
-});
