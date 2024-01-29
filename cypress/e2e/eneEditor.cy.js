00bb30a9633b8ebcbad550425fbf0a42ec4f886f
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-01-28 21:11:34 -0700

    Update eneEditor.cy.js

--- cypress/e2e/eneEditor.cy.js
+++ cypress/e2e/eneEditor.cy.js
@@ -1,90 +1,56 @@
-function presentDefaultContent(toggleElements = {}) {
-  // FIXME: A function that checks for the default content to be present. Modify if developing
-  //  new default content for the page i.e removal or addition of new UI element.
+describe('Supply Chain Editor Tests', () => {
+    it('should create and fill out multiple nodes in the supply chain', () => {
+        cy.visit('http://localhost/dist/edit.html'); 
 
-  const defaultVisibility = {
-    '#minfodetail': false,
-    '#viz-choices': true,
-    '#measure-choices': true,
-    //'#timeCapture': false,
-    '#mapcapture': true,
-    '#timeSlider': true,
-    //'#viz-slider': true,
-    '#searchbar': true,
-    '#fullscreen-menu': true,
-    '#mapmenu': true,
-    '#mapmenu-window': false,
-  };
+        cy.get('div.form-control input[type="text"][name="root[summary][name]"]')
+            .clear()
+            .type('Test Supply Chain');
 
-  const finalVisibility = { ...defaultVisibility, ...toggleElements };
+        cy.get('div[data-schemapath="root.summary.description"] .CodeMirror')
+            .first()
+            .then(editor => {
+                const codeMirror = editor[0].CodeMirror;
+                codeMirror.setValue('This is a detailed description of the supply chain.');
+            });
 
-  Object.keys(finalVisibility).forEach(selector => {
-    if (finalVisibility[selector]) {
-      cy.get(selector).should('be.visible');
-    } else {
-      cy.get(selector).should('not.be.visible');
-    }
-  });
-}
+        cy.get('input[name="root[location][address]"]')
+            .clear()
+            .type('New York, NY');
 
-describe('Map Layer Toggle Tests', () => {
-  it('should toggle map layers', () => {
-    cy.visit('http://localhost/dist/edit.html');
-    //https://manifest.supplystudies.com/
-    //https://manifest.supplystudies.com/dev
+        cy.get('input.geocoderinput[name="root[location][geocode]"]')
+            .clear()
+            .type('40.730610,-73.935242');
 
-    cy.get('div.form-control input[type="text"][name="root[summary][name]"]')
-        .clear()
-        .type('Test Supply Chain')
+        for (let nodeIndex = 0; nodeIndex < 3; nodeIndex++) {
+            cy.get('.json-editor-btn-add[title="Add Node"]').click();
 
-    cy.get('div[data-schemapath="root.summary.description"] .CodeMirror')
-        .first()
-        .then(editor => {
-          const codeMirror = editor[0].CodeMirror;
-          codeMirror.setValue(''); 
+            cy.get(`input[name="root[nodes][${nodeIndex}][overview][name]"]`)
+                .type(`Node ${nodeIndex + 1}`);
 
-          // Apply bold formatting
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-bold').click();
-          codeMirror.replaceSelection('**Bold Text**\n\n');
+            cy.get('.CodeMirror').eq(nodeIndex + 1).then(editor => {
+                const codeMirror = editor[0].CodeMirror;
+                codeMirror.setValue(`Detailed description for Node ${nodeIndex + 1}.`);
+            });
 
-          // Apply italic formatting
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-italic').click();
-          codeMirror.replaceSelection('*Italic Text*\n\n');
+            cy.get(`#Location`).click();
+            cy.wait(1000);
+            cy.get(`input[name="root[nodes][${nodeIndex}][location][address]"]`).type('456 Warehouse Ave', { force: true });
+            cy.get(`input[name="root[nodes][${nodeIndex}][location][geocode]"]`).type('40.730610,-73.935242', { force: true });
 
-          // Apply heading
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-header').click();
-          codeMirror.replaceSelection('# Heading Text\n\n');
+            cy.get(`#Attributes`).click();
+            cy.wait(1000);
 
-          // Apply quote
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-quote-left').click();
-          codeMirror.replaceSelection('> Quote Text\n\n');
+            cy.get(`#Measures`).click();
+            cy.wait(1000);
 
-          // Apply unordered list
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-list-ul').click();
-          codeMirror.replaceSelection('* List Item 1\n* List Item 2\n\n');
+            cy.get(`#Notes`).click();
+            cy.wait(1000);
+            cy.get(`div[data-schemapath="root.nodes.${nodeIndex}.notes.markdown"] .CodeMirror`).then(editor => {
+                const codeMirror = editor[0].CodeMirror;
+                codeMirror.setValue(`Additional notes for Node ${nodeIndex + 1}.`);
+            });
 
-          // Apply ordered list
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-list-ol').click();
-          codeMirror.replaceSelection('1. First Item\n2. Second Item\n\n');
 
-          // Apply link
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-link').click();
-          codeMirror.replaceSelection('[Example Link](https://example.com)\n\n');
-
-          // Apply image
-          cy.get('div[data-schemapath="root.summary.description"] .editor-toolbar .fa-picture-o').click();
-          codeMirror.replaceSelection('![Example Image](https://example.com/image.png)\n\n');
-        });
-
-    cy.get('input[name="root[location][address]"]')
-        .click() 
-        .type('New York, NY');
-
-    const coordinates = "40.730610,-73.935242";
-
-    cy.get('input.geocoderinput[name="root[location][geocode]"]')
-        .click()
-        .type(coordinates)
-
-  });
+        }
+    });
 });
