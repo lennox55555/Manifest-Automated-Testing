7584f46f61a9ab36908413069a2c1f5ea4f027aa
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-01-28 21:13:04 -0700

    Update eneDatasets.cy.js

--- cypress/e2e/eneDatasets.cy.js
+++ cypress/e2e/eneDatasets.cy.js
@@ -1,6 +1,27 @@
-describe('Visualization Option Tests', () => {
-  it('should change visualization options', () => {
-    cy.visit('http://localhost/dist/');
-    cy.get('#viz-choices').select('Chord');
+describe('Data Page Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost/dist/data.html');
   });
+
+  it('checks navigation links', () => {
+    cy.get('#minfomenu .hb-menu-item').each($el => {
+      const href = $el.prop('href');
+      expect(href).not.to.be.empty;
+    });
+  });
+
+
+  it('tests search functionality', () => {
+    cy.get('#datasearch').type('search query');
+  });
+
+  it('tests pagination', () => {
+    cy.get('.pagination .page').first().click();
+  });
+
+  it('tests external links', () => {
+    cy.get('a[href^="http"]').each($el => {
+    });
+  });
+
 });
