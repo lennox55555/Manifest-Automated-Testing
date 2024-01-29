ddcc2573c859a9e1178d0c746a36e52e350bd7a2
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2024-01-28 21:11:57 -0700

    Update eneAbout.cy.js

--- cypress/e2e/eneAbout.cy.js
+++ cypress/e2e/eneAbout.cy.js
@@ -1,9 +1,16 @@
-describe('Manifest Bar Tests', () => {
-  it('should toggle the manifest bar', () => {
-    cy.visit('http://localhost/dist/');
-    cy.get('#minfo-hamburger').click();
-    cy.get('#minfodetail').should('be.visible');
-    cy.get('#minfo-hamburger').click();
-    cy.get('#minfodetail').should('not.be.visible');
+describe('About Page Tests', () => {
+  it('should display all elements and verify links', () => {
+    cy.visit('http://localhost/dist/about.html');
+
+    cy.get('#infoborder').should('exist');
+    cy.get('#minfoheader').should('exist');
+    cy.get('#minfodetail').should('exist');
+    cy.get('h1').contains('About Manifest').should('be.visible');
+    cy.get('.splash-image').should('exist');
+
+    cy.get('a[href="https://supplystudies.com/syllabus/#reversesourcing"]').should('be.visible').and('have.attr', 'href', 'https://supplystudies.com/syllabus/#reversesourcing');
+    cy.get('a[href="https://github.com/hock/Manifest"]').should('be.visible').and('have.attr', 'href', 'https://github.com/hock/Manifest');
+
+    cy.get('a[rel="license"]').should('be.visible').and('have.attr', 'href', 'http://creativecommons.org/licenses/by/4.0/');
   });
 });
